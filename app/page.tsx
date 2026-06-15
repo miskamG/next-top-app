502fa3b654c159a8436adcb99034dd100102f28a
a.galeev <a.galeev@malltech.ru>
2026-06-15 18:00:12 +0500

    add tag component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "./components/Button/Button";
+import { P } from "./components/P/P";
 
 export default function Home() {
   return (
     <>
-      <Button appearance="primary" arrow="rightZz">Primary</Button>
+      <Button appearance="primary" arrow="right">Primary</Button>
       <Button appearance="ghost">Ghost</Button>
+      <P size="l">Большой</P>
     </>
   );
 }
